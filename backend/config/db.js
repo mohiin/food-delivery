83dcda652915b1d569fa152a417b75e6893ca129
Mohin Uddin <mdmohinuddin4721@gmail.com>
2024-12-23 16:40:54 +0600

    hide atlas credentials

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,8 +1,17 @@
 import mongoose from "mongoose";
+import dotenv from 'dotenv';
+// Load environment variables from the .env file
+dotenv.config();
+
+const dbUrl = process.env.ATLASDB_URL;
 
 export const connnectDB  = async()=>{
-    await mongoose.connect("mongodb+srv://patwary10:CRRHvnW7qJ6VGrg6@cluster0.pkksc.mongodb.net/foodDel")
+    await mongoose.connect(dbUrl)
     .then(()=>{
         console.log("DB is connected successfully");
+    })
+    .catch((err)=>{
+        console.log(err);
+        console.log("Some error in DB");
     })
 }
