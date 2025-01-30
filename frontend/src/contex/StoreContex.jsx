a9e0cb6c281f1b83a9a2113c0333ebc323f17233
Mohin <mdmohinuddin4721@gmail.com>
2025-01-30 12:06:23 +0600

    Add online backend url

--- frontend/src/contex/StoreContex.jsx
+++ frontend/src/contex/StoreContex.jsx
@@ -6,7 +6,7 @@ export const StoreContex = createContext(null);
 const StoreContexProvider = (props) => {
 
     const [cartItems, setCartItems] = useState({});
-    const url = "http://localhost:4000";
+    const url = "https://food-delivery-backend-65jm.onrender.com";
     const [token, setToken] = useState("");
 
     const [food_list, setFood_list] = useState([]);
